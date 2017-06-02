ef02718ac4bf6041f070c52a1790a41c70885281
Harakan <wgt538@gmail.com>
2017-06-02 16:29:14 -0500

    Start the webserver script

---
+++ start_webserver.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games"
+
+#This starts a webserver in a screen
+# -S name of screen
+# -d -m  is make it no matter what
+# command of bash -c "RUN WHATEVER BASH HERE YOU WANT"
+pushd /home/pi/gits/aura-website/
+screen -S webserver -d -m bash -c "python -m SimpleHTTPServer 80"
+popd

4ddfc5608ce60235d2925a5ca411c0c10cbfc6c9
Harakan <wgt538@gmail.com>
2017-06-02 16:29:50 -0500

    Now pointing at the index.html

--- start_webserver.sh
+++ start_webserver.sh
@@ -6,6 +6,6 @@ PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/ga
 # -S name of screen
 # -d -m  is make it no matter what
 # command of bash -c "RUN WHATEVER BASH HERE YOU WANT"
-pushd /home/pi/gits/aura-website/
+pushd /home/pi/gits/aura-website/src/html/
 screen -S webserver -d -m bash -c "python -m SimpleHTTPServer 80"
 popd

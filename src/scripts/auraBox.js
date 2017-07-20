fc393cbb740bf56936b60845c69b785b1d2d2ebb
Wynston <wramsay@hotmail.ca>
2017-07-20 15:21:34 -0600

    Syncing beacons from firebase to local

--- src/scripts/auraBox.js
+++ src/scripts/auraBox.js
@@ -1,8 +1,29 @@
-//controls the bootstrap modal content
+//AuraBox local module controller
 auraCreate.auraBox = function($scope, $http){
 
 	//displays the local hosted webcam view from the AuraBox
 	$scope.displayAuraBoxWebcamModal = function(){
 		$("#AuraBoxWebcamModal").modal();
+	}
+
+	//syncs a beacon's object assets to local from firebase
+	$scope.syncBeacon = function(beacon){
+		var assetsToSync = [];
+		for(var i = 0; i < $scope.objectsArray[i].length; i++){
+			if(beacon.beacon_id == $scope.objectsArray[i].beacon_id){
+				for(var j = 0; j < $scope.objectsArray[i].assets[j]; j++){
+					assetsToSync.push($scope.objectsArray[i].assets[j].value);
+				}
+			}
+		}
+		$http({
+        method: 'POST',
+        url: "http://127.0.0.1:8080",
+        data: assetsToSync
+		}).then(function mySuccess(response) {
+			alertSuccess("SUCCESS: the beacon " + name + " has been locally synced!");
+		}, function myError(response) {
+		    alertFailure("Error: failed to sync the beacon " + name + ".\n" + response.statusText);
+		});
 	}
 }

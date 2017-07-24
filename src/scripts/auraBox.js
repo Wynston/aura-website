679e441fd70c1ac208995cf7a7126cef71ac9bd5
Wynston <wramsay@hotmail.ca>
2017-07-24 11:25:20 -0600

    Sending data as JSON in local upload

--- src/scripts/auraBox.js
+++ src/scripts/auraBox.js
@@ -9,7 +9,6 @@ auraCreate.auraBox = function($scope, $http){
 	//syncs a beacon's object assets to local from firebase
 	$scope.syncBeacon = function(beacon){
 		var assetsToSync = [];
-		var assetData = new FormData();
 		for(var i = 0; i < $scope.objectsArray[i].length; i++){
 			if(beacon.beacon_id == $scope.objectsArray[i].beacon_id){
 				for(var j = 0; j < $scope.objectsArray[i].assets[j]; j++){
@@ -17,12 +16,11 @@ auraCreate.auraBox = function($scope, $http){
 				}
 			}
 		}
-		assetData.append('file', "THIS_IS_A_TEST.txt");
 
 		$http({
         method: 'POST',
         url: "https://10.0.1.1/uploadmod",
-        data: assetData
+        data: JSON.toString(assetData)
 		}).then(function mySuccess(response) {
 			alertSuccess("SUCCESS: the beacon " + beacon.beacon_name + " has been locally synced!");
 		}, function myError(response) {
